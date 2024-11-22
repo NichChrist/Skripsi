649fd85115a1d4452aee83391861da65f2ea72d4
NichChrist <ncone10@gmail.com>
2024-11-22 15:50:03 +0700

    🎸  added logging system for debuging

--- app.py
+++ app.py
@@ -63,6 +63,37 @@ def preprocess_text(text):
     lemmas = [wnl.lemmatize(token, pos=get_wordnet_pos(token)) for token in tokens]
     return ' '.join(lemmas)
 
+#Def for every pre-processing return
+def evaluation_preprocess_text(text):
+    # 1. Case Folding
+    text_lower = text.lower()
+
+    # 2. Removing Non-ASCII Characters
+    text_ascii = ''.join(char for char in text_lower if ord(char) < 128)
+
+    # 3. Removing Punctuation
+    text_no_punct = text_ascii.translate(str.maketrans('', '', string.punctuation))
+
+    # 4. Stop Word Removal (excluding negation words)
+    stop_words = set(stopwords.words('english'))
+    negation_words = {"not", "no", "dont", "didnt", "doesnt", "cant", "couldnt", "wont", "wouldnt", "shouldnt", "wasnt", "werent", "havent", "hasnt", "hadnt", "aint"}
+    stop_words.difference_update(negation_words)
+    tokens = word_tokenize(text_no_punct)
+    text_no_stopwords = ' '.join([word for word in tokens if word not in stop_words])
+
+    # 5. Lemmatization
+    wnl = WordNetLemmatizer()
+
+    def get_wordnet_pos(word):
+        tag = nltk.pos_tag([word])[0][1][0].upper()
+        tag_dict = {"J": wordnet.ADJ, "N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV}
+        return tag_dict.get(tag, wordnet.NOUN)
+
+    lemmas = [wnl.lemmatize(token, pos=get_wordnet_pos(token)) for token in tokens]
+    text_lemmatized = ' '.join(lemmas)
+
+    return [text_lower, text_ascii, text_no_punct, text_no_stopwords, text_lemmatized]    
+
 # universal Def for Prediction Format
 def format_prediction(prediction):
     if prediction >= 0.5:
@@ -83,37 +114,9 @@ def get_top_n_words(text, n=10):
 def index():
     return render_template('index.html')
 
-#Singular Predict Page
-@app.route('/single')
-def singular():
-    return render_template('klasifikasi-single.html')
-
-#Singular Predict Return
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = request.get_json()
-        text = data.get('text')
-        if not text:
-            return jsonify({'error': 'No text provided'}), 400
-
-        preprocessed_text = preprocess_text(text)
-        prediction = model.predict([preprocessed_text])
-        sentiment = format_prediction(prediction[0][0])
-
-        return jsonify({'prediction': sentiment, 'preprocessing': preprocessed_text})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-#Multiple Predict Page
-@app.route('/klasifikasi-multiple')
-def klasifikasi_multiple():
-    return render_template('klasifikasi-multiple.html')
-
-#Multiple Predict Return
-@app.route('/multiple-predict', methods=['POST'])
-def multiple_predict():
+#Main Page Return
+@app.route('/evaluate')
+def evalutate():
     try:
         # Check the request file
         if 'file' not in request.files:
@@ -166,6 +169,105 @@ def multiple_predict():
                 "top_positive_words": positive_word_counts.most_common(10),
                 "top_negative_words": negative_word_counts.most_common(10)
             }
+
+            return jsonify(result)
+
+        except Exception as e:
+            return jsonify({"error": f"Error reading Excel file or during prediction: {e}"}), 500
+
+        finally: #remove temp file
+            os.remove(file_path)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+#Singular Predict Page
+@app.route('/single')
+def singular():
+    return render_template('klasifikasi-single.html')
+
+#Singular Predict Return
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        text = data.get('text')
+        if not text:
+            return jsonify({'error': 'No text provided'}), 400
+
+        preprocessed_text = preprocess_text(text)
+        prediction = model.predict([preprocessed_text])
+        sentiment = format_prediction(prediction[0][0])
+
+        return jsonify({'prediction': sentiment, 'preprocessing': preprocessed_text})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+#Multiple Predict Page
+@app.route('/klasifikasi-multiple')
+def klasifikasi_multiple():
+    return render_template('klasifikasi-multiple.html')
+
+#Multiple Predict Return
+@app.route('/multiple-predict', methods=['POST'])
+def multiple_predict():
+    try:
+        # Check the request file
+        if 'file' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+        # Store as temp file
+        file_uuid = str(uuid.uuid4())  #unique filename
+        filename = file_uuid + ".xlsx" #save as excel
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Now use the multiple_predict logic 
+        try:
+            df = pd.read_excel(file_path)
+            texts = df['content'].tolist()
+
+            logging.info(f"Extracted reviews: {texts}")
+
+            review = []
+            predictions = []
+            positive_words = []
+            negative_words = []
+
+            for x in texts:
+                try:
+                    # Predict
+                    preprocessed_text = preprocess_text(x)
+                    prediction = model.predict([preprocessed_text])
+                    sentiment = format_prediction(prediction)
+                    # Store the review and the predict result
+                    review.append(x)
+                    predictions.append(sentiment)
+                    # Count the words    
+                    if sentiment == "Positive":
+                        positive_words.extend(get_top_n_words(x))
+                    elif sentiment == "Negative":
+                        negative_words.extend(get_top_n_words(x))
+
+                except Exception as e:
+                    print(f"Error during prediction for text '{x}': {e}")
+                    predictions.append("Error")
+                    preprocessed_texts.append("Error")
+
+            positive_word_counts = Counter(word for word, count in positive_words)
+            negative_word_counts = Counter(word for word, count in negative_words)
+
+            result = {
+                "predictions": predictions,
+                "review": review,
+                "top_positive_words": positive_word_counts.most_common(10),
+                "top_negative_words": negative_word_counts.most_common(10)
+            }
+
+            logging.info(f"Prediction successful: {result}")
 
             return jsonify(result)
 
@@ -236,6 +338,9 @@ def scraping_predict():
                 "top_positive_words": positive_word_counts.most_common(10),
                 "top_negative_words": negative_word_counts.most_common(10)
             }
+
+            logging.info(f"Prediction successful: {result}")
+
             return jsonify(result)
 
         except Exception as e:
